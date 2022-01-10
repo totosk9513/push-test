668ad48fbbb9b99612fef8acebb5d4e7a1a3fdec
ReeWill_N2O <83605020+totosk9513@users.noreply.github.com>
2022-01-10 16:50:52 -0500

    ㄴㄴㄴ

---
+++ src/test/mein.java
@@ -0,0 +1,16 @@
+package test;
+
+public class mein 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Commit test");
+		System.out.println("Meow");
+		//under there is korean. to check utp-8 encoding
+		//여기서 부터 한국어. utp-8 인코딩 테스트
+		System.out.println("커밋 테스트");
+		System.out.println("냐옹");
+		//aabbss
+	
+	}
+}
